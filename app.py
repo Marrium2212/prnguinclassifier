8164d1d44e5484358130ca70288eb9618738d8cd
Marrium2212 <118329753+Marrium2212@users.noreply.github.com>
2025-07-30 09:45:44 -0600

    Update app.py

--- app.py
+++ app.py
@@ -68,11 +68,30 @@ data = {'island':island,
 input_df = pd.DataFrame(data, index=[0])
 input_penguins = pd.concat([input_df,X_raw], axis=0)
 
-with st.expander("Input data")
+with st.expander("Input data"):
 st.write("**Input data**")
 input_df
 st.write("**Combined data**"):
  st.write("**Input data**")
+
+input_penguins
+encode = ['island','sex']
+df_penguins =pd.get_dummies(input_penguines, prefix = encode)
+X = df_penguins[1:]
+input_row = df_penguins[:1]
+
+# One hot encoding for y
+target_mapper = {
+ 'Adelie': 0,
+ 'Chinstrap': 1,
+ 'Gentoo': 2
+}
+
+def target_encode(val):
+ return target_mapper[val]
+
+y =y_raw.apply(target_encode)
+
 
         
  
