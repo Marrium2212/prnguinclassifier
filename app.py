8acd5ded26943c104d0a2292d0c00e950db8beed
Marrium2212 <118329753+Marrium2212@users.noreply.github.com>
2025-07-30 08:51:04 -0600

    Create app.py

---
+++ app.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import streamlit as st
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+ 
+st.title("Penguine Speicy Prediction ML app")
+st.info("This is end-to-end Machine Learning App")
+ 
+with st.expander("Data"):
+  pass
+ 
+with st.expander("Data Visualization"):
+  pass
+ 
+with st.expander("Input data"):
+  pass
+ 
+with st.expander("Data Preperation"):
+  pass
+ 
+with st.sidebar:
+  st.header("Input Variables")
+  island = st.selectbox('Island',('Biscoe','Dream','Torgersen'))
